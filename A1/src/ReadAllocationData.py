f7a97f02cd49c192a9c385fee6bff7f9777e88c1
Alice Ka-Ying Ip <ipa1@mills.cas.mcmaster.ca>
2019-01-13 22:43:27 -0500

    Completed readFreeChoice(s)

--- A1/src/ReadAllocationData.py
+++ A1/src/ReadAllocationData.py
@@ -24,3 +24,15 @@ def readStdnts(s):
 		sRecord = dict(macid=sLine[0], fname=sLine[1], lname=sLine[2], gender=sLine[3], gpa=sLine[4], choies=[sLine[5],sLine[6],sLine[7]])
 		allRecords.append(sRecord);
 	return allRecords
+
+## @brief Formats a file input into a list of macids of students who get free choice
+#  @param s string corresponding to a filename
+#  @return list of macIds
+def readFreeChoice(s):
+	freeList=[]
+	file = open(s, "r")
+	for line in file:
+		sLine = line.strip()
+		sLine = sLine.split(" ")
+		freeList.append(sLine[0])
+	return freeList
