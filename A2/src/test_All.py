709f4737e1c3e34b05ead61feff2d6d958224cb5
Alice <ipa1@mcmaster.ca>
2019-02-11 21:47:16 -0500

    edited test_All tests

--- A2/src/test_All.py
+++ A2/src/test_All.py
@@ -9,10 +9,7 @@ from DCapALst import *
 from AALst import *
 from SALst import *
 
-class TestSALst:
-	def test_zero_division(self):
-		SALst.init()
-		with raises(ValueError):
-			SALst.sort(lambda t: t.freechoice and t.gpa >= 4.0)
-
-
+class TestRead:
+	def setup_method(self, method):
+		stdntf = load_stdnt_data("StdntData.txt")
+		deptf = load_dcap_data("DeptCap.txt")
