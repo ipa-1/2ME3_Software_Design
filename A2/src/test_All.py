f016c474b7a79b5a48eaca68eaf4c7f4f771c736
Alice <ipa1@mcmaster.ca>
2019-02-11 21:27:29 -0500

    added a test

--- A2/src/test_All.py
+++ A2/src/test_All.py
@@ -2,3 +2,17 @@
 #  @author Alice Ip ipa1
 #  @brief
 #  @date 2019-02-09
+from pytest import *
+from StdntAllocTypes import *
+from SeqADT import *
+from DCapALst import *
+from AALst import *
+from SALst import *
+
+class TestSALst:
+	def test_zero_division(self):
+		SALst.init()
+		with raises(ValueError):
+			SALst.sort(lambda t: t.freechoice and t.gpa >= 4.0)
+
+
