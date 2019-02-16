76285ccfe2cdd6496115d9561dc42b718977dff7
Alice <ipa1@mcmaster.ca>
2019-02-16 17:13:07 -0500

    Fixed AALst.py num_alloc

--- A2/src/A2Examples.py
+++ A2/src/A2Examples.py
@@ -7,7 +7,7 @@ from SALst import *
 DCapALst.init()
 DCapALst.add(DeptT.civil, 2)
 
-sinfo1 = SInfoT("first", "last", GenT.male, 12.0, SeqADT([DeptT.civil, DeptT.chemical]), True)
+sinfo1 = SInfoT("first", "last", GenT.male, 12.0, SeqADT([DeptT.civil, DeptT.chemical]), False)
 
 SALst.init()
 SALst.add("stdnt1", sinfo1)
