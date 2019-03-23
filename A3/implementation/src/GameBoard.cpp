46ac9b983d98fe65e8c411af977ae670863f5143
Alice <ipa1@mcmaster.ca>
2019-03-23 17:00:03 -0400

    Added comments to GameBoard.cpp

--- A3/implementation/src/GameBoard.cpp
+++ A3/implementation/src/GameBoard.cpp
@@ -81,9 +81,11 @@ bool BoardT::is_valid_waste_mv(CategoryT c, nat n) const
 	if (W.size() == 0)
 		return false;
 
-
-
 	if (c == Tableau){
+
+		if(!(is_valid_pos(Tableau, n))) // check that tableau argument is in range
+			throw std::out_of_range("waste_mv tableau argument out of range");
+
 		if (T[n].size() > 0)
 			return (W.top().s == T[n].top().s && W.top().r == T[n].top().r+1); //return valid_waste_tab(n);
 		else
@@ -91,6 +93,10 @@ bool BoardT::is_valid_waste_mv(CategoryT c, nat n) const
 	}
 		
 	else if (c == Foundation){
+		
+		if(!(is_valid_pos(Foundation,n))) // check that foundation argument is in range
+			throw std::out_of_range("waste_mv foundation argument out of range");
+
 		if (F[n].size() > 0)
 			return (W.top().s == F[n].top().s && W.top().r == F[n].top().r+1); //return valid_waste_foundation(n);
 		else
@@ -106,10 +112,10 @@ bool BoardT::is_valid_deck_mv() const
 	return (D.size() > 0); // Deck move is always valid unless there is nothing in deck
 };
 
-
+// Performs a tab move
 void BoardT::tab_mv(CategoryT c, nat n0, nat n1)
 { 
-	if (!(is_valid_tab_mv(c,n0,n1)))
+	if (!(is_valid_tab_mv(c,n0,n1))) // Checks if the tab mv to c is valid
 		throw std::invalid_argument("Invalid tab_mv argument");
 
 	if (c == Tableau){
@@ -125,30 +131,26 @@ void BoardT::tab_mv(CategoryT c, nat n0, nat n1)
 	}
 };
 
-
+// Performs a waste move
 void BoardT::waste_mv(CategoryT c, nat n)
 {
 	if (!(is_valid_waste_mv(c,n))) // checks if move is valid
 		throw std::invalid_argument("Invalid waste_mv argument");
 
 	if (c == Tableau){
-		if(!(is_valid_pos(Tableau, n))) // check that tableau argument is in range
-			throw std::out_of_range("waste_mv tableau argument out of range");
 		CardT topCard = W.top();
 		W = W.pop();
 		T[n] = T[n].push(topCard);
 	}
 
 	else if (c == Foundation){
-		if(!(is_valid_pos(Foundation,n))) // check that foundation argument is in range
-			throw std::out_of_range("waste_mv foundation argument out of range");
 		CardT topCard = W.top();
 		W = W.pop();
 		F[n] = F[n].push(topCard);		
 	}
 };
 
-
+// Performs a deck move
 void BoardT::deck_mv()
 {
 	if (!(is_valid_deck_mv()))
@@ -159,6 +161,7 @@ void BoardT::deck_mv()
 };
 
 
+// Gets a tableau card stack
 CardStackT BoardT::get_tab(nat n) const
 {
 	if (!(is_valid_pos(Tableau,n)))
@@ -166,8 +169,7 @@ CardStackT BoardT::get_tab(nat n) const
 	return T[n];
 }
 
-
-
+//Gets a foundation card stack
 CardStackT BoardT::get_foundation(nat n) const
 {
 	if (!(is_valid_pos(Foundation,n)))
@@ -175,63 +177,71 @@ CardStackT BoardT::get_foundation(nat n) const
 	return F[n];
 };
 
-
+// Gets the deck card stack
 CardStackT BoardT::get_deck() const
 {
 	return D;
 };
 
+//Gets the waste card stack
 CardStackT BoardT::get_waste() const
 {
 	return W;
 };
 
-
+//Checks to see if a valid move exists
 bool BoardT::valid_mv_exists() const
 {
-	return (valid_tab_mv() || valid_waste_mv() || is_valid_deck_mv());
+	return (valid_tab_mv() || valid_waste_mv() || is_valid_deck_mv()); // checks if there are valid tab, waste, deck moves
 };
 
+//Checks to see if the game is in a win state
 bool BoardT::is_win_state() const
 {
-	for (int i = 0; i < 8; i++)
-		if (!(F[i].size() == 13))
+	for (int i = 0; i < 8; i++) // For every foundation
+		if (!(F[i].size() == 13)) // If the size of the foundation is not 13, the size of a suit, then there are cards missing
 			return false;
 	return true;
 };
 
 //Private Accessors
 
+//Checks to see if there is a valid move from tab to any other Category T
 bool BoardT::valid_tab_mv() const
 {
-	for (int i = 0;i < 10;i++){
-		if (!(is_valid_pos(Tableau, i))) // For each Tableau, check the origin is valid
+	for (int i = 0;i < 10;i++){ // For each Tableau
+		if (!(is_valid_pos(Tableau, i))) // Check that the position of this tableau is valid
 			continue;
-		for (int j = 0; j < 8; j++){ // For each Foundation, check destination valid, and mv valid
-			if (is_valid_pos(Foundation, j) && is_valid_tab_mv(Foundation, i,j))
+		for (int j = 0; j < 8; j++){ // For each Foundation, 
+			if (is_valid_pos(Foundation, j) && is_valid_tab_mv(Foundation, i,j)) // Check position of foundation, and move
 				return true;
 		}
 	}
 
-	for (int i = 0;i < 10;i++){
-		if (!(is_valid_pos(Tableau, i))) // For each Tableau, check the origin is valid
+	for (int i = 0;i < 9;i++){ // For each Tableau,
+		if (!(is_valid_pos(Tableau, i)))  // Check check the position is valid
 			continue;
-		for (int j = 0; j < 8; j++){ // For each Foundation, check destination valid, and mv valid
-			if (is_valid_pos(Foundation, j) && is_valid_tab_mv(Foundation, i,j))
+		for (int j = i+1; j < 10; j++){ // For each unchecked tableau 
+			if (is_valid_pos(Foundation, j) && is_valid_tab_mv(Foundation, i,j)) // Check position and move
 				return true;
 		}
 	}
 
-	return false; // if it never passed all three tests in either for loop, then no moves available
+	return false; // if it never passed all three tests in either for loop, then no moves available from tableau
 };
 
+//Checks to see if there is a valid move from waste to any other CategoryT
 bool BoardT::valid_waste_mv() const
 {
-	for (int i = 0; i < 8; i++){
+	if (W.size() <= 0){ // If the waste card stack size is 0, there are no possible waste moves
+		return false;
+	}
+
+	for (int i = 0; i < 8; i++){ // Checking for waste to foundation moves
 		if (is_valid_pos(Foundation, i) && is_valid_waste_mv(Foundation, i))
 			return true;
 	}
-	for (int i = 0; i < 10; i++){
+	for (int i = 0; i < 10; i++){ // Checking for waste to tableau moves
 		if (is_valid_pos(Tableau, i) && is_valid_waste_mv(Tableau, i))
 			return true;
 	}
@@ -239,28 +249,31 @@ bool BoardT::valid_waste_mv() const
 
 };
 
+//Checks to make sure the desired position is within range
 bool BoardT::is_valid_pos(CategoryT t, nat n) const
 {
-	if (t == Tableau)
+	if (t == Tableau) // Checking if the position is valid for tableau
 		return (n >= 0 && n <= 9);
-	else if (t == Foundation)
+	else if (t == Foundation) // Checking if the position is valid for foundation
 		return (n >= 0 && n <= 7);
 	else
 		return true;
 };
 
+//Checks to make sure that the suit and rank of a tableau to foundation move is valide
 bool BoardT::valid_tab_foundation(nat n0, nat n1) const
 {
 	if (T[n0].size() > 0){
-		if (F[n1].size() > 0)
-			return (T[n0].top().s == F[n1].top().s && T[n0].top().r == F[n1].top().r+1);
-		else
+		if (F[n1].size() > 0) // If there is a card on foundation already
+			return (T[n0].top().s == F[n1].top().s && T[n0].top().r == F[n1].top().r+1); // Check suit and rank
+		else // If there are no cards on foundation, any card can be moved on
 			return true;
 	}
-	else
+	else // Tableau size is 0, no moves possible if there are no cards
 		return false;
 }
 
+//Checks to make sure that the suit and rank of a tableau to tableau move is valid
 bool BoardT::valid_tab_tab(nat n0, nat n1) const{
 	if (T[n0].size() > 0){
 		if (T[n1].size() > 0){ // check suits are same, and target is one rank smaller
