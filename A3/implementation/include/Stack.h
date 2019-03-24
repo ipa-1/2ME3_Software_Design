d0d516646d2645abb966905cdeb4d2a8c1873215
Alice <ipa1@mcmaster.ca>
2019-03-24 15:20:33 -0400

    Finished doxygen for stack.h

--- A3/implementation/include/Stack.h
+++ A3/implementation/include/Stack.h
@@ -1,5 +1,10 @@
 //Alice Ip ipa1
 
+/**
+ *  \file Stack.h
+ *  \brief A class that represents a generic stack with typical stack functions
+ */
+
 #ifndef A3_STACK_H_
 #define A3_STACK_H_
 #include <vector>
@@ -12,14 +17,47 @@ template <class T>// Stack constructor
 class Stack{
 
 	private:
+		/// A vector to store a sequence
 		std::vector<T> stack;
 
 	public:
+	    /**
+	     *  \brief Constructor for the stack
+	     *  \param cs a vector representing the 
+	     *   initial values in the stack
+	     */
 		Stack(std::vector<T> cs) ;
+
+	    /**
+	     *  \brief Adds a item of the type T to the stack
+	     *  \param item, the item to add to the stack
+	     *  \return A new stack with the desired item added
+	     */
 		Stack<T> push(T item) const;
+
+	    /**
+	     *  \brief Pops an item from the stack
+	     *  \return A new stack with the top value 
+	     *   popped off of the stack
+	     */
 		Stack<T> pop() const;
+
+	    /**
+	     *  \brief Obtains the top value of stack and returns it
+	     *  \return item on the top of the stack
+	     */
 		T top() const;
+
+	    /**
+	     *  \brief Obtains the size of the stack and returns it
+	     *  \return Size of the current stack
+	     */
 		nat size() const;
+
+	    /**
+	     *  \brief Returns the vector in the stack
+	     *  \return the vector stored in the stack
+	     */
 		std::vector<T> toSeq() const;
 };
 #endif
